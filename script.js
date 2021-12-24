94b9e2c5dd47e8911ed7335abf95852dd372c70c
yuri000117 <96627652+yuri000117@users.noreply.github.com>
2021-12-25 01:00:26 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function showpopup () {
+  var Popup = document.getElementById('popup')
+  Popup.classList.add('show')
+}
+
+function hidepopup () {
+  var Popup = document.getElementById('popup')
+  Popup.classList.remove('show')
+}
